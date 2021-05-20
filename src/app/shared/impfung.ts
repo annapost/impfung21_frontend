bea71116c79f3d7c2ffa28a181497fc79d00e49e
annapost <postlmayr.anna@gmail.com>
2021-05-20 13:42:52 +0200

    1 file modified

--- src/app/shared/impfung.ts
+++ src/app/shared/impfung.ts
@@ -3,9 +3,9 @@
 export class Impfung {
   constructor (
     public id:number, 
-    public datum:date, 
+    public datum:Date, 
     public max_personen:number, 
-    public uhrzeit_start:time, 
-    public uhrzeit_end:time, 
+    public uhrzeit_start:Time, 
+    public uhrzeit_end:Time, 
     public impfort_id:Impfort){}
 }
