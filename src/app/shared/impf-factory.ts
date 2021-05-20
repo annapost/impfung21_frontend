dd81455eb473585b419f7fb3569dac85e19bbed3
annapost <postlmayr.anna@gmail.com>
2021-05-20 19:19:49 +0200

    2 files modified

--- src/app/shared/impf-factory.ts
+++ src/app/shared/impf-factory.ts
@@ -4,22 +4,19 @@ export class ImpfFactory {
 
  static empty(): Impfung {
    //legt neue Dummy Impfung an
-   return new Impfung(null, new Date(), 0, new Date(), new Date(), null, [{id: 0, plz: 0, ort: '', strasse: '', bezeichnung: ''}], []);
+   return new Impfung(null, new Date(), 0, new Date(), new Date(), null, {id: 0, plz: 0, ort: '', strasse: '', bezeichnung: '' }, []);
  }
 
  static fromObject(rawImpfung: any): Impfung {
    return new Impfung(
      rawImpfung.id,
      typeof(rawImpfung.datum) === 'string' ?
-       new Date(rawImpfung.datum) : rawImpfung.datum,
+        new Date(rawImpfung.datum) : rawImpfung.datum,
      rawImpfung.max_personen,
-
      typeof(rawImpfung.uhrzeit_start) === 'string' ?
-       new Date(rawImpfung.uhrzeit_start) : rawImpfung.uhrzeit_start,
-
+        new Date(rawImpfung.uhrzeit_start) : rawImpfung.uhrzeit_start,
      typeof(rawImpfung.uhrzeit_end === 'string' ? 
-     new Date(rawImpfung.uhrzeit_end) : rawImpfung.uhrzeit_end,
-
+        new Date(rawImpfung.uhrzeit_end) : rawImpfung.uhrzeit_end,
      rawImpfung.impfort_id,
      rawImpfung.impfort,
      rawImpfung.users,
--- src/app/shared/impf-system.service.ts
+++ src/app/shared/impf-system.service.ts
@@ -1,17 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 import {Impfung, Impfort, User} from "./impfung";
 
 @Injectable()
 export class ImpfSystemService {
-  impfungen: Impfung[];
+  private api = "https://impfung21.s1810456026.student.kwmhgb.at/api";
+  constructor(private http:HttpClient) { }
 
-  constructor() {
-
+  getAll():Observable<Array<Impfung>>{
+    return this.http.get<Array<Impfung>>(`${this.api}/impfungen`).
+      pipe(retry(3)).pipe(catchError(this.errorHandler));
   }
 
-  getAll(){
-    return this.impfungen;
+  getSingle(isbn:string):Observable<Impfung>{
+    return this.http.get<Impfung>(`${this.api}/impfungen/${isbn}`).
+      pipe(retry(3)).pipe(catchError(this.errorHandler));
   }
+
+  private errorHandler(error: Error | any): Observable<any> { return throwError(error);
+}
 
 }
 
