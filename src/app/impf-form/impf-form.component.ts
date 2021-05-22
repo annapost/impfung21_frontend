75d981ee005852b10698a86a326baf7a62559269
annapost <postlmayr.anna@gmail.com>
2021-05-22 17:03:59 +0200

    2 files modified

--- src/app/impf-form/impf-form.component.ts
+++ src/app/impf-form/impf-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'im-impf-form',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: []
 })
 export class ImpfFormComponent implements OnInit {
+  impfForm: FormGroup;
 
   constructor() { }
 
